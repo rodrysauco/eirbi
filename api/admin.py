b487bb80c63fbf369d8965025a09a9f1790f4c5a
francolisotti <francolisotti96@gmail.com>
2018-10-25 20:46:01 -0300

    First steppes

--- api/admin.py
+++ api/admin.py
@@ -2,12 +2,23 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import (FechaAlq, Propiedad, Anfitrion, Ciudad, Reserva)
+from .models import (FechaAlq, Propiedad, Anfitrion, Ciudad,
+                     Reserva)
 
 # Register your models here.
+
+class FechaAlquiler_Inline (admin.TabularInline):
+    model = FechaAlq
+    fk_name = "propiedad"
+    max_num = 7
+
+
+class PropiedadAdmin (admin.ModelAdmin):
+    inlines = [FechaAlquiler_Inline, ]
+
+#Models registered in the site
 admin.site.register(FechaAlq)
-admin.site.register(Propiedad)
+admin.site.register(Propiedad, PropiedadAdmin)
 admin.site.register(Anfitrion)
 admin.site.register(Ciudad)
 admin.site.register(Reserva)
-
--- api/models.py
+++ api/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-
-
-class FechaAlq (models.Model):
-    fecha = models.DateField
-
-
-class Propiedad (models.Model):
-    descripcion = models.TextField(max_length=200)
-    imagen = models.ImageField
-    tarifaDiaria = models.IntegerField
-    fecha = models.ForeignKey(FechaAlq, on_delete=models.CASCADE)
+from django.utils import timezone
 
 
 class Anfitrion (models.Model):
@@ -17,10 +7,33 @@ class Anfitrion (models.Model):
     telefono = models.BigIntegerField
     domicilio = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Ciudad (models.Model):
     nombre = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nombre
+
+
+class Propiedad (models.Model):
+    title = models.CharField(max_length=25, default=None)
+    descripcion = models.TextField(max_length=200)
+    propietario = models.ForeignKey(Anfitrion,on_delete=models.CASCADE, default=None)
+    imagen = models.ImageField
+    tarifaDiaria = models.IntegerField
+    ciudad = models.ForeignKey(Ciudad,on_delete=models.CASCADE, default=None)
+
 
 class Reserva (models.Model):
-    fecha = models.ForeignKey(FechaAlq, on_delete=models.CASCADE)
+    fechaDeReserva = models.DateField(default = timezone.now)
+    numeroReserva = models.IntegerField
+    propiedad = models.ForeignKey(Propiedad,on_delete=models.CASCADE, default=None)
+
+
+class FechaAlq (models.Model):
+    fecha = models.DateField
+    propiedad = models.ForeignKey(Propiedad,on_delete=models.CASCADE, default= None)
+    reserva = models.ForeignKey(Reserva,on_delete=models.CASCADE, default = None)
